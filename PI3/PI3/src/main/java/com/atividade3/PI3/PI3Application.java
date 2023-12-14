3d089bde7a12846e1b46d8453494810a78234e19
Lucas Akira <akira034567@gmail.com>
2023-12-14 15:32:05 -0300

    Atividade2

---
+++ PI3/PI3/src/main/java/com/atividade3/PI3/PI3Application.java
@@ -0,0 +1,11 @@
+package com.atividade3.PI3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PI3Application {
+	public static void main(String[] args) {
+		SpringApplication.run(PI3Application.class, args);
+	}
+}
---
+++ PI3/PI3/src/test/java/com.atividade3.PI3/PI3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.atividade3.PI3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PI3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
